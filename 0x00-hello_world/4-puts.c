6b87908138bf4839cfe858c07f4eeb75fada95c0
yraeonti <imuetinyanbello@gmail.com>
2023-02-16 17:19:45 -0800

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 #include<string.h>
-
 /* 
  * main - Entry point
  * Return: 0
  */
+
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
+	
 	return (0);
 }	
