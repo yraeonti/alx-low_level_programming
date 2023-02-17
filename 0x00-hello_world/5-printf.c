9f699fa4ea334f89410399e56eb54ac179af0f82
yraeonti <imuetinyanbello@gmail.com>
2023-02-16 17:45:37 -0800

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
  * main - entry point
- * return 0
+ * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
