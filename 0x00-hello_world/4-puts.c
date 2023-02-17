848c200d3096c4b07a973f7f2663b6cd5b9d2de6
yraeonti <imuetinyanbello@gmail.com>
2023-02-16 16:57:54 -0800

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-  char str[] = "Programming is like building a multilingual puzzle";
-
-	puts(str);
-
-	return (0);
+/* Returns */
+int main(void)
+{
+char str[] = "\"Programming is like building a multilingual puzzle";
+puts(str);
+return (0);
 }	
