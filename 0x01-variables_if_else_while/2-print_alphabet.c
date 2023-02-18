c9f1c7fe2aad17269582c2f4336da34a0812e43b
yraeonti <imuetinyanbello@gmail.com>
2023-02-18 08:19:05 -0800

    new activity

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<ctype.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	putchar(tolower(ch));
+	return (0);
+}
