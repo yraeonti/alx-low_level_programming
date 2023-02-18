83f5f576376b5adf3675b0e6f54bda60abad9901
yraeonti <imuetinyanbello@gmail.com>
2023-02-18 10:55:02 -0800

    new activity

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<ctype.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	putchar('0' + i);
+	}
+	putchar('\n');
+	return (0);
+}
