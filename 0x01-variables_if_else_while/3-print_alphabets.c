f24726f4421acf2659d2e40533a65e1986dafe70
yraeonti <imuetinyanbello@gmail.com>
2023-02-18 09:39:23 -0800

    print characters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<ctype.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	char alp[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	i = 0;
+	while (alp[i])
+	{
+	putchar(tolower(alp[i]));
+	putchar(alp[i]);
+	i++;
+	}
+	putchar('\n');
+	return (0);
+}
