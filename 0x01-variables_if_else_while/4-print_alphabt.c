2e4600a6df9b687706e2e0aa4182d9a8322617bc
yraeonti <imuetinyanbello@gmail.com>
2023-02-18 10:26:04 -0800

    new activity

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<ctype.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	char alp[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	i = 0;
+	while (alp[i])
+	{
+	if (alp[i] == 'Q' || 'E')
+	{
+	;
+	}
+	else
+	{
+	putchar(tolower(alp[i]));
+	}
+	}
+	putchar('/n');
+	return (0);
+}
