e220736a1865abb774ba60dbc8ba7d3bfafecfb3
yraeonti <imuetinyanbello@gmail.com>
2023-02-18 06:43:51 -0800

    new activity

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry Point
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n > 0)
+	{
+	printf("%i is positive", n);
+	}
+	else if (n == 0)
+	{
+	printf("0 is zero");
+	}
+	else if(n < 0)
+	{
+	printf("%i is negative", n);
+	}
+	return (0);
+}
