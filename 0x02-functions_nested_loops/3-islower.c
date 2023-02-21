15a9d2de0319b46e7ee7094c78c05750a80c51cd
yraeonti <imuetinyanbello@gmail.com>
2023-02-21 14:10:55 -0800

    new nested

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<ctype.h>
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * Return: int
+ */
+
+int _islower(int c)
+{
+	if (tolower(c) == c)
+	{
+	return (1)
+	}
+	else
+	{
+	return (0)
+	}
+}
