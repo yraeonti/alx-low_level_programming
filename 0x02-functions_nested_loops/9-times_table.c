afb606c51ecd15fb8badafcfd99559fd722f8928
yraeonti <imuetinyanbello@gmail.com>
2023-02-22 02:34:23 -0800

    new works

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,12 +14,23 @@ void times_table(void)
 {
 	int h;
 	int m;
+	int mul;
 
 	for (h = 0; h <= 9; h++)
 	{
 	for (m = 0; m <= 9; m++)
 	{
-	printf("%i * %i", h, m);
+	mul = h * m;
+	
+	if (mul < 10)
+	{
+	_putchar('0' + mul);
+	}
+	else if (mul >= 10)
+	{
+	_putchar((mul / 10) + '0');
+	_putchar((mul % 10) + '0');
+	}
 	_putchar(' ');
 	_putchar(',');
 	_putchar('\n');
