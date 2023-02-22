85ecd8778ade838b24f3a14dd312ee033f23cd9b
yraeonti <imuetinyanbello@gmail.com>
2023-02-22 00:57:51 -0800

    new works

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * @n: int
+ * Return: int
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	last_digit = n % 10;
+
+	return (last_digit);
+}
