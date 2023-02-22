a15390e6b5108c0f00a58d0014059f208dbdeba1
yraeonti <imuetinyanbello@gmail.com>
2023-02-22 01:40:04 -0800

    new works

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<ctype.h>
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: int
+ */
+
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h <= 23; h++)
+	{
+	for (m = 0; m <= 59; m++)
+	{
+	if (h < 10 && m < 10)
+	{
+	_putchar(0);
+	_putchar(h);
+	_putchar(':');
+	_putchar(0);
+	_putchar(m);
+	}
+	else if (h < 10  && m >= 10)
+	{
+	_putchar(0);
+	_putchar(h);
+	_putchar(':');
+	_putchar(m);
+	}
+	else if (h >= 10 && m < 10)
+	{
+	_putchar(h);
+	_putchar(':');
+	_putchar(0);
+	_putchar(m);
+	}
+	else if (h >= 10 && m >= 10)
+	{
+	_putchar(h);
+	_putchar(':');
+	_putchar(m);
+	}
+	}
+	}
+}
