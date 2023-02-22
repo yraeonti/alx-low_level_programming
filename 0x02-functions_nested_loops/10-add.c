b243091c2dcd95d7af6ba94cbd45d2b90ff311d8
yraeonti <imuetinyanbello@gmail.com>
2023-02-22 03:00:23 -0800

    new works

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<ctype.h>
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
