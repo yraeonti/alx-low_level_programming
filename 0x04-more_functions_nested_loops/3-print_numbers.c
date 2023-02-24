e15f59b7be1f5034cd08d48790df268b771fc420
yraeonti <imuetinyanbello@gmail.com>
2023-02-24 15:35:57 -0800

    more functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - entry point
+ * 
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	putchar(i);
+	}
+}
