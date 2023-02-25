f2a094b011a3ec6bc1d14523738d7b0a4eba4038
yraeonti <imuetinyanbello@gmail.com>
2023-02-24 16:22:39 -0800

    more functions

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - entry point
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+	for (j = 0; j < 15; j++)
+	{
+	_putchar('0'+ j);
+	}
+	putchar('\n');
+	}
+}
