be4b5e099de2e0ffa0de6adc0380dff3263dbde7
yraeonti <imuetinyanbello@gmail.com>
2023-02-24 16:42:51 -0800

    more functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,7 +14,15 @@ void more_numbers(void)
 	{
 	for (j = 0; j < 15; j++)
 	{
+	if (j < 10)
+	{
 	putchar('0' + j);
+	}
+	else
+	{
+	_putchar((j / 10) + '0');
+	_putchar((j % 10) + '0');
+	}
 	}
 	putchar('\n');
 	}
