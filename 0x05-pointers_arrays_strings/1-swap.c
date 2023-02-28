33e85fd35459d458a9853da8d85e2bac064404d9
yraeonti <imuetinyanbello@gmail.com>
2023-02-28 02:08:49 -0800

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - entry point
+ * @a: pointer to int
+ * @b: pointer to int
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a, *b = *b, *a;
+}
