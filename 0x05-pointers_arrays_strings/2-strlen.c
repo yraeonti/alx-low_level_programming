d882d8d2f53501810a3d81c7f0b60e18afa08bfb
yraeonti <imuetinyanbello@gmail.com>
2023-02-28 03:22:34 -0800

    arrays and pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+#include "main.h"
+
+/**
+ * swap_int - entry point
+ * @s: pointer to char 
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	return strlen(s);
+}
