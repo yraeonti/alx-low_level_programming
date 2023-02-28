b828bd245df213b38edbf69d3e8fa16897971914
yraeonti <imuetinyanbello@gmail.com>
2023-02-28 07:17:53 -0800

    array and pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+#include "main.h"
+
+/**
+ * _strcpy - entry point
+ * @dest: pointer to char
+ * @src: pointer to char
+ * Return: pointer to char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
