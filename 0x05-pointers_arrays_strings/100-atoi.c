d7974b407e8ee86ff80a0273734e8c9f1a50e521
yraeonti <imuetinyanbello@gmail.com>
2023-02-28 07:26:48 -0800

    array and pointers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "main.h"
+
+/**
+ * _atoi - entry point
+ * @s: pointer to char
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
