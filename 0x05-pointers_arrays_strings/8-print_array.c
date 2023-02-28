af8392bd9ee4d8b4b1409758dfb5ffc0f81b7dd7
yraeonti <imuetinyanbello@gmail.com>
2023-02-28 06:53:21 -0800

    array and pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include "main.h"
+
+/**
+ * print_array - entry point
+ * @a: pointer to int
+ * @n: int
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	printf("%d, ", a[i]);
+	}
+	putchar('\n');
+}
