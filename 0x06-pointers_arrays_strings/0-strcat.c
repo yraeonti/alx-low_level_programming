82aefc51ec4e78e2e58a272124d464e10b221a3e
yraeonti <imuetinyanbello@gmail.com>
2023-03-02 14:41:06 -0800

    more apointers and arrays

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+#include "main.h"
+
+/**
+ * _strcat - entry point
+ * @dest: pointer to char
+ * @src: pointer to char
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
