50968d970e1bfb3d51d148a8a18b17eeb36961d2
yraeonti <imuetinyanbello@gmail.com>
2023-03-02 16:59:06 -0800

    more pointers and arrays

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include<ctype.h>
+#include "main.h"
+/**
+ * string_toupper - Entry point
+ * @a: integer array to be reversed
+ * Return: char
+ */
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; i != '\0'; ++i)
+	{
+	a[i] = toupper(a[i]);
+	}
+	return (a);
+}
