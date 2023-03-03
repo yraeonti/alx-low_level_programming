5b12e78152600805926719e24c7cec0d1374778c
yraeonti <imuetinyanbello@gmail.com>
2023-03-02 16:23:42 -0800

    more pointers and arrays

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,9 +6,9 @@
  * _strcmp - entry point
  * @s1: pointer to char
  * @s2: pointer to char
- * Return: char
+ * Return: int
  */
-char _strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	return (strcmp(s1, s2));
 }
