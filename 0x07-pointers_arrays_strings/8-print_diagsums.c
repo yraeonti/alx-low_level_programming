1c612ce5476b0616e2ef25787ba3c3bab2e944e4
yraeonti <imuetinyanbello@gmail.com>
2023-03-07 07:17:29 -0800

    8 diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include<string.h>
+#include "main.h"
+/**
+ * print_diagsums - entry point
+ * @a: two dimentional array
+ * @size: int
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j;
+	long int leftSum, rightSum;
+
+	leftSum = 0;
+	rightSum = 0;
+
+	for (i = 0, j = 0; i < size && j < size; i++, j++)
+	{
+	leftSum += a[i][j];
+	}
+	for (i = 0, j = (size - 1); i < size && j > 0; i++, j--)
+	{
+	rightSum += a[i][j];
+	}
+	printf("%li, %li", leftSum, rigtSum);
+}
