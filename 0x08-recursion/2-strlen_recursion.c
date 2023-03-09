cf356f931b855fdfbd8f1d169dd5e53e738759f4
yraeonti <imuetinyanbello@gmail.com>
2023-03-09 04:32:26 -0800

    task 3

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,13 +7,9 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i;
-
-	i = 0;
 	if (*s == '\0')
 	{
 	return (0);
 	}
-	i++;
-	return (i + _strlen_recursion(s + 1)); 
+	return (1 + _strlen_recursion(s + 1));
 }
