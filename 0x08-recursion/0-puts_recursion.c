cfd9c1940ba7ec49d0984b013ec7aaed87be83f8
yraeonti <imuetinyanbello@gmail.com>
2023-03-09 02:30:16 -0800

    task 0 recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - entry point
+ * @s: pointer to char
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	return (puts(s));
+}
