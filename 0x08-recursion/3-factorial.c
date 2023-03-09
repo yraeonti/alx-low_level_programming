a56c8d7301c91f3e4b202f485bc2585f7e1bde4e
yraeonti <imuetinyanbello@gmail.com>
2023-03-09 05:03:06 -0800

    task 3 factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include<string.h>
+#include "main.h"
+/**
+ * factorial - entry point
+ * @n: int
+ * Return: int
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
