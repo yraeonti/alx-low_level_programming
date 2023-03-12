c2165d1cd167d81ebd7b765df351a81d3e6c4048
yraeonti <imuetinyanbello@gmail.com>
2023-03-12 11:19:29 -0700

    arguments to main task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: pointer to char
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[argc - argc]);
+return (0);
+}
