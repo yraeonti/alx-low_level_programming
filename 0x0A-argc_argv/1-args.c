879530a21df9a3655238dd34e1296ddc3494fd53
yraeonti <imuetinyanbello@gmail.com>
2023-03-12 11:46:35 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: pointer to char
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
