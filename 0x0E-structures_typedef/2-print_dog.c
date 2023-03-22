62e24dd39e2d53cedf569d2d5317b485561f8576
yraeonti <imuetinyanbello@gmail.com>
2023-03-22 08:59:58 -0700

    structs

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+  * print_dog - Initializes a dog structure
+  * @d: struct of dog
+  * Return: Nothing
+  */
+void print_dog(struct dog *d)
+}
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	if (d->age == NULL)
+	}
+		printf("Age: (nil)\n");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+}
