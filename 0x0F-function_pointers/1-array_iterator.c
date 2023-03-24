c04c02a7473b70226e455c3caaa588f26b3848c4
yraeonti <imuetinyanbello@gmail.com>
2023-03-24 12:25:38 -0700

    array_iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,15 +1,18 @@
 #include "function_pointers.h"
-
+#include<stdio.h>
 /**
   * array_iterator - entry point
-  * @array: array 
+  * @array: array
   * @size: size of array
   * @action: pointer to fuc
   */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (size < 0)
+	if (size == 0)
 		;
+	else
+	{
 	array_iterator(array, size - 1, action);
-	action(array[size]);
+	action(array[size - 1]);
+	}
 }
