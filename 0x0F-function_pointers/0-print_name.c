206c15d8cfaa70b21535baf514cf4a59d9a6b8ef
yraeonti <imuetinyanbello@gmail.com>
2023-03-24 11:14:59 -0700

    corrections

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,11 @@
  * print_name - entry point
  * @name: pointer to char
  * @f: pointer to function
- * Return: 0
+ * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
